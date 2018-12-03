caa174562418285b8d9b1cd4262d3f4b16fc179d
Julien WITTOUCK <julien.wittouck@gmail.com>
2018-12-03 19:47:37 +0100

    :sparkles: day 1 - part 2

--- src/01-chronal-calibration.js
+++ src/01-chronal-calibration.js
@@ -1,11 +1,10 @@
+const functions = {
+    "+" : (a,b) => a+b,
+    "-" : (a,b) => a-b
+};
+
 function computeFrequency(frequencies){
     let result = 0;
-
-
-    const functions = {
-        "+" : (a,b) => a+b,
-        "-" : (a,b) => a-b
-    };
 
     frequencies.forEach(freq => {
        let operator = functions[freq.charAt(0)];
@@ -16,17 +15,41 @@ function computeFrequency(frequencies){
     return result;
 }
 
-function solve(){
-    let myInput = require("fs").readFileSync("data/01-chronal-calibration.txt").toString();
+function computeFrequencyUntilRepetition(frequencies){
+    let result = 0;
 
-    const part1 = computeFrequency(myInput.split("\n"));
+    const alreadySeenFrequencies = new Set();
+    alreadySeenFrequencies.add(result);
+
+    for(let i = 0; ; i++) {
+        let freq = frequencies[i % frequencies.length];
+        let operator = functions[freq.charAt(0)];
+        let operand = parseInt( freq.substr(1) );
+        result = operator(result, operand);
+        if (alreadySeenFrequencies.has(result)){
+            return result;
+        }
+
+        alreadySeenFrequencies.add(result);
+    }
+}
+
+function solve(){
+    const myInput = require("fs").readFileSync("data/01-chronal-calibration.txt").toString();
+    const frequencies = myInput.split("\n");
+
+    const part1 = computeFrequency(frequencies);
+    const part2 = computeFrequencyUntilRepetition(frequencies)
 
     console.log("--- Day 1: Chronal Calibration ---");
     console.log(`Puzzle answer : ${part1}`);
+    console.log("--- Part Two ---");
+    console.log(`Puzzle answer : ${part2}`);
     console.log();
 }
 
 module.exports = {
     computeFrequency,
+    computeFrequencyUntilRepetition,
     solve
 };
--- test/01-chronal-calibration.js
+++ test/01-chronal-calibration.js
@@ -1,30 +1,74 @@
 const expect = require("chai").expect;
 
 const computeFrequency = require("../src/01-chronal-calibration").computeFrequency;
+const computeFrequencyUntillRepetition = require("../src/01-chronal-calibration").computeFrequencyUntilRepetition;
 
 describe("--- Day 1: Chronal Calibration ---", () => {
 
-    const examples = [
-        {
-            "frequencies": "+1, +1, +1",
-            "result": 3
-        },
-        {
-            "frequencies": "+1, +1, -2",
-            "result": 0
-        },
-        {
-            "frequencies": "-1, -2, -3",
-            "result": -6
-        }
+    describe("--- Part One ---", () => {
 
-    ];
+        const examples = [
+            {
+                "frequencies": "+1, -2, +3, +1",
+                "result": 3
+            },
+            {
+                "frequencies": "+1, +1, +1",
+                "result": 3
+            },
+            {
+                "frequencies": "+1, +1, -2",
+                "result": 0
+            },
+            {
+                "frequencies": "-1, -2, -3",
+                "result": -6
+            }
 
-    it("should compute device frequencies", ()=>{
-        examples.forEach(example => {
-            let result = computeFrequency(example.frequencies.split(", "));
-            expect(result).to.equal(example.result);
-        })
+        ];
+
+
+        it("should compute device frequencies", ()=>{
+            examples.forEach(example => {
+                let result = computeFrequency(example.frequencies.split(", "));
+                expect(result).to.equal(example.result);
+            })
+        });
+
+    });
+
+    describe("--- Part Two ---", () => {
+
+        const examples = [
+            {
+                "frequencies": "+1, -2, +3, +1",
+                "result": 2
+            },
+            {
+                "frequencies": "+1, -1",
+                "result": 0
+            },
+            {
+                "frequencies": "+3, +3, +4, -2, -4",
+                "result": 10
+            },
+            {
+                "frequencies": "-6, +3, +8, +5, -6",
+                "result": 5
+            },
+            {
+                "frequencies": "+7, +7, -2, -7, -4",
+                "result": 14
+            }
+        ];
+
+        it("should compute device frequencies", ()=>{
+            examples.forEach(example => {
+                let result = computeFrequencyUntillRepetition(example.frequencies.split(", "));
+                expect(result).to.equal(example.result);
+            })
+        });
+
     });
 
 });
