7dab00f8bbb645d3eba8e3463bb1a929d6793261
Julien WITTOUCK <julien.wittouck@gmail.com>
2018-12-05 19:57:35 +0100

    :sparkles: day 3 - part 2

--- src/03-no-matter-how-you-slice-it.js
+++ src/03-no-matter-how-you-slice-it.js
@@ -11,7 +11,8 @@ function parseClaim(claim){
         size: {
             width: parseInt(width),
             height: parseInt(height)
-        }
+        },
+        overlaps: false
     }
 }
 
@@ -50,19 +51,76 @@ function claimFabric(fabric, claim){
     }
 }
 
+function overlaps(claimA, claimB){
+    // find if the two rectangles overlap?!
+    let rectangleA = {
+        pointA: {
+            x: claimA.position.left,
+            y: claimA.position.top
+        },
+        pointB: {
+            x: claimA.position.left + claimA.size.width - 1,
+            y: claimA.position.top + claimA.size.height - 1
+        }
+    };
+    let rectangleB = {
+        pointA: {
+            x: claimB.position.left,
+            y: claimB.position.top
+        },
+        pointB: {
+            x: claimB.position.left + claimB.size.width - 1,
+            y: claimB.position.top + claimB.size.height - 1
+        }
+    };
+    if(rectangleB.pointA.x > rectangleA.pointB.x){
+        return false;
+    }
+    if(rectangleB.pointA.y > rectangleA.pointB.y){
+        return false;
+    }
+    if(rectangleB.pointB.x < rectangleA.pointA.x){
+        return false;
+    }
+    if(rectangleB.pointB.y < rectangleA.pointA.y){
+        return false;
+    }
+    return true;
+}
+
+function findNonOverlapping(claims){
+    for(let claimA of claims) {
+        for (let claimB of claims) {
+            if (claimA.id === claimB.id) {
+                continue;
+            }
+            if(overlaps(claimA, claimB)){
+                claimA.overlaps = true;
+                claimB.overlaps = true;
+            }
+        }
+    }
+    return claims.find(claim => claim.overlaps === false);
+}
+
 function solve(){
     const myInput = require("fs").readFileSync("data/03-no-matter-how-you-slice-it.txt").toString();
     const claims = myInput.split("\n");
 
     const part1 = computeOverlapping(claims);
+    const part2 = findNonOverlapping(claims.map(parseClaim)).id;
 
     console.log("--- Day 3: No Matter How You Slice It ---");
     console.log(`Puzzle answer : ${part1}`);
+    console.log("--- Part Two ---");
+    console.log(`Puzzle answer : ${part2}`);
     console.log();
 }
 
 module.exports = {
     parseClaim,
     computeOverlapping,
+    overlaps,
+    findNonOverlapping,
     solve
 };
--- test/03-no-matter-how-you-slice-it.js
+++ test/03-no-matter-how-you-slice-it.js
@@ -1,17 +1,17 @@
 const expect = require("chai").expect;
 
-const {parseClaim, computeOverlapping} = require("../src/03-no-matter-how-you-slice-it");
+const {parseClaim, computeOverlapping, overlaps, findNonOverlapping} = require("../src/03-no-matter-how-you-slice-it");
 
 
 describe("--- Day 3: No Matter How You Slice It ---", () => {
 
-    describe("--- Part One ---", () => {
+    const claims = [
+        "#1 @ 1,3: 4x4",
+        "#2 @ 3,1: 4x4",
+        "#3 @ 5,5: 2x2"
+    ];
 
-        const claims = [
-            "#1 @ 1,3: 4x4",
-            "#2 @ 3,1: 4x4",
-            "#3 @ 5,5: 2x2"
-        ];
+    describe("--- Part One ---", () => {
 
         it("should parse a claim", () => {
            let result = parseClaim("#123 @ 3,2: 5x4");
@@ -27,6 +27,23 @@ describe("--- Day 3: No Matter How You Slice It ---", () => {
             expect(result).to.equal(4);
         });
 
+    });
+
+    describe("--- Part Two ---", () => {
+
+        it("should detect when claims overlaps", () => {
+            const claimA = parseClaim(claims[0]);
+            const claimB = parseClaim(claims[1]);
+            const claimC = parseClaim(claims[2]);
+            expect(overlaps(claimA, claimB)).to.equal(true);
+            expect(overlaps(claimB, claimC)).to.equal(false);
+            expect(overlaps(claimA, claimC)).to.equal(false);
+        });
+
+        it("should find the claim that doesn't overlap", () => {
+            let result = findNonOverlapping(claims.map(parseClaim));
+            expect(result.id).to.equal(3);
+        });
     });
 
 });
