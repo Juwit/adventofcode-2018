59d8006be82b7733e2d3d37c5340bc8f22231d09
Julien WITTOUCK <julien.wittouck@gmail.com>
2018-12-23 19:27:14 +0100

    :sparkles: day 7 - part 2

--- src/07-the-sum-of-its-parts.js
+++ src/07-the-sum-of-its-parts.js
@@ -1,16 +1,17 @@
 const requirementRegex = /Step (\w) must be finished before step (\w) can begin./;
 
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 function parse(input){
     return input.split("\n")
         .map(requirement => {
            const [_, from, to] = requirementRegex.exec(requirement);
            return {from, to};
         })
-}
 
+}
 function dependencyMap(requirements){
     const map = new Map();
-    // init the map
     requirements.forEach(({from, to}) => {
         if(! map.has(from)){
             map.set(from, new Set());
@@ -21,11 +22,10 @@ function dependencyMap(requirements){
         map.get(to).add(from);
     });
     return map;
-}
 
+}
 function nextStep(dependencyMap){
-    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    return chars.split("").find(char => {
+    return alphabet.split("").find(char => {
         const dependencies = dependencyMap.get(char);
         return dependencies && dependencies.size === 0;
     });
@@ -45,16 +45,77 @@ function order(dependencyMap){
     return result;
 }
 
+function computeTime(step){
+    return alphabet.split("").indexOf(step) + 1;
+}
+
+function computeSteps(dependencyMap, timeOffset, workersCount){
+    let ticks = 0;
+
+    let workers = new Map();
+
+    while(dependencyMap.size !== 0){
+
+        // if a worker is available
+        while( workers.size < workersCount && nextStep(dependencyMap) !== undefined){
+            let step = nextStep(dependencyMap);
+            dependencyMap.delete(step);
+            workers.set(step, computeTime(step) + timeOffset);
+        }
+
+        for(step of workers.keys()){
+            let time = workers.get(step);
+            time--;
+            workers.set(step, time);
+
+            if(time === 0){
+                // step finished !
+                workers.delete(step);
+                for(value of dependencyMap.values()){
+                    value.delete(step);
+                }
+            }
+
+        }
+
+        ticks++;
+    }
+
+    // finishing ongoing steps
+    while(workers.size !== 0 ){
+
+        for(step of workers.keys()){
+
+            let time = workers.get(step);
+            time--;
+            workers.set(step, time);
+
+            if(time === 0){
+                // step finished !
+                workers.delete(step);
+            }
+            ticks++;
+        }
+    }
+
+    return ticks;
+}
+
+
 
 function solve(){
     const myInput = require("fs").readFileSync("data/07-the-sum-of-its-parts.txt").toString();
 
     const requirements = parse(myInput);
-    const map = dependencyMap(requirements);
+    let map = dependencyMap(requirements);
     const part1 = order(map);
+    map = dependencyMap(requirements);
+    const part2 = computeSteps(map, 60, 5);
 
     console.log("--- Day 7: The Sum of Its Parts ---");
     console.log(`Puzzle answer : ${part1}`);
+    console.log("--- Part Two ---");
+    console.log(`Puzzle answer : ${part2}`);
     console.log();
 }
 
@@ -63,5 +124,7 @@ module.exports = {
     dependencyMap,
     nextStep,
     order,
+    computeTime,
+    computeSteps,
     solve
 };
--- test/07-the-sum-of-its-parts.js
+++ test/07-the-sum-of-its-parts.js
@@ -1,12 +1,10 @@
 const expect = require("chai").expect;
 
-const {parse, dependencyMap, nextStep, order} = require("../src/07-the-sum-of-its-parts");
+const {parse, dependencyMap, nextStep, order, computeTime, computeSteps} = require("../src/07-the-sum-of-its-parts");
 
 describe("--- Day 7: The Sum of Its Parts ---", () => {
 
-    describe("--- Part One ---", () => {
-
-        const input = `Step C must be finished before step A can begin.
+    const input = `Step C must be finished before step A can begin.
 Step C must be finished before step F can begin.
 Step A must be finished before step B can begin.
 Step A must be finished before step D can begin.
@@ -14,15 +12,17 @@ Step B must be finished before step E can begin.
 Step D must be finished before step E can begin.
 Step F must be finished before step E can begin.`;
 
-        const requirements = [
-            {from:"C", to:"A"},
-            {from:"C", to:"F"},
-            {from:"A", to:"B"},
-            {from:"A", to:"D"},
-            {from:"B", to:"E"},
-            {from:"D", to:"E"},
-            {from:"F", to:"E"}
-        ];
+    const requirements = [
+        {from:"C", to:"A"},
+        {from:"C", to:"F"},
+        {from:"A", to:"B"},
+        {from:"A", to:"D"},
+        {from:"B", to:"E"},
+        {from:"D", to:"E"},
+        {from:"F", to:"E"}
+    ];
+
+    describe("--- Part One ---", () => {
 
         const map = new Map();
         map.set("A",new Set(["C"]));
@@ -51,6 +51,22 @@ Step F must be finished before step E can begin.`;
     });
 
     describe("--- Part Two ---", () => {
+
+        const map = new Map();
+        map.set("A",new Set(["C"]));
+        map.set("B",new Set(["A"]));
+        map.set("C",new Set());
+        map.set("D",new Set(["A"]));
+        map.set("E",new Set(["B", "D", "F"]));
+        map.set("F",new Set(["C"]));
+
+        it("should complete step C in 3 seconds", () => {
+           expect(computeTime("C")).to.equal(3);
+        });
+
+        it("should complete all steps in 15 seconds with 2 workers", () => {
+           expect(computeSteps(map, 0, 2)).to.equal(15);
+        });
 
     });
 
