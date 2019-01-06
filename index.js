f7c7da5a0566421f29a3501f8c759356c117959b
Julien WITTOUCK <julien.wittouck@gmail.com>
2019-01-06 18:39:28 +0100

    :recycle: refactors index script

--- index.js
+++ index.js
@@ -1,74 +1,17 @@
+const fs = require("fs");
+
+function solveDay(number){
+    const dayPart1 = require(`./src/${number}/part1`);
+    const dayPart2 = require(`./src/${number}/part2`);
+    console.log(dayPart1.title);
+    console.log(`Puzzle answer : ${dayPart1.solve()}`);
+    console.log("--- Part Two ---");
+    console.log(`Puzzle answer : ${dayPart2.solve()}`);
+    console.log();
+}
+
 console.log("[Advent of Code 2018]");
 console.log();
 
-const day01Part1 = require("./src/01/part1");
-const day01Part2 = require("./src/01/part2");
-console.log(day01Part1.title);
-console.log(`Puzzle answer : ${day01Part1.solve()}`);
-console.log("--- Part Two ---");
-console.log(`Puzzle answer : ${day01Part2.solve()}`);
-
-console.log();
-
-const day02Part1 = require("./src/02/part1");
-const day02Part2 = require("./src/02/part2");
-console.log(day02Part1.title);
-console.log(`Puzzle answer : ${day02Part1.solve()}`);
-console.log("--- Part Two ---");
-console.log(`Puzzle answer : ${day02Part2.solve()}`);
-
-console.log();
-
-const day03Part1 = require("./src/03/part1");
-const day03Part2 = require("./src/03/part2");
-console.log(day03Part1.title);
-console.log(`Puzzle answer : ${day03Part1.solve()}`);
-console.log("--- Part Two ---");
-console.log(`Puzzle answer : ${day03Part2.solve()}`);
-
-console.log();
-
-const day04Part1 = require("./src/04/part1");
-const day04Part2 = require("./src/04/part2");
-console.log(day04Part1.title);
-console.log(`Puzzle answer : ${day04Part1.solve()}`);
-console.log("--- Part Two ---");
-console.log(`Puzzle answer : ${day04Part2.solve()}`);
-
-console.log();
-
-const day05Part1 = require("./src/05/part1");
-const day05Part2 = require("./src/05/part2");
-console.log(day05Part1.title);
-console.log(`Puzzle answer : ${day05Part1.solve()}`);
-console.log("--- Part Two ---");
-console.log(`Puzzle answer : ${day05Part2.solve()}`);
-
-console.log();
-
-const day06Part1 = require("./src/06/part1");
-const day06Part2 = require("./src/06/part2");
-console.log(day06Part1.title);
-console.log(`Puzzle answer : ${day06Part1.solve()}`);
-console.log("--- Part Two ---");
-console.log(`Puzzle answer : ${day06Part2.solve()}`);
-
-console.log();
-
-const day07Part1 = require("./src/07/part1");
-const day07Part2 = require("./src/07/part2");
-console.log(day07Part1.title);
-console.log(`Puzzle answer : ${day07Part1.solve()}`);
-console.log("--- Part Two ---");
-console.log(`Puzzle answer : ${day07Part2.solve()}`);
-
-console.log();
-
-const day08Part1 = require("./src/08/part1");
-const day08Part2 = require("./src/08/part2");
-console.log(day08Part1.title);
-console.log(`Puzzle answer : ${day08Part1.solve()}`);
-console.log("--- Part Two ---");
-console.log(`Puzzle answer : ${day08Part2.solve()}`);
-
-console.log();
+fs.readdirSync("src")
+    .forEach(solveDay);
